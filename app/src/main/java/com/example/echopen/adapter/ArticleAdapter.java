eb1538a21daf5a4e91536bb09b947d7445aa01b1
Mayesha <mim2007088@stud.kuet.ac.bd>
2024-05-23 12:35:44 +0600

    Adapter design pattern in Article Section

--- app/src/main/java/com/example/echopen/adapter/ArticleAdapter.java
+++ app/src/main/java/com/example/echopen/adapter/ArticleAdapter.java
@@ -1,101 +1,5 @@
-//package com.example.echopen.adapter;
-//
-//
-//import android.content.Context;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//
-//import androidx.annotation.NonNull;
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//import com.bumptech.glide.Glide;
-//import com.example.echopen.Model.BlogItemModel;
-//import com.example.echopen.databinding.ArticleItemBinding;
-//
-//import java.util.List;
-//
-//public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.BlogViewHolder> {
-//
-//    private final Context context;
-//    private List<BlogItemModel> blogList;
-//    private final OnItemClickListener itemClickListener;
-//
-//    public interface OnItemClickListener {
-//        void onEditClick(BlogItemModel blogItem);
-//        void onReadMoreClick(BlogItemModel blogItem);
-//        void onDeleteClick(BlogItemModel blogItem);
-//    }
-//
-//    public ArticleAdapter(Context context, List<BlogItemModel> blogList, OnItemClickListener itemClickListener) {
-//        this.context = context;
-//        this.blogList = blogList;
-//        this.itemClickListener = itemClickListener;
-//    }
-//
-//    @NonNull
-//    @Override
-//    public BlogViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-//        ArticleItemBinding binding = ArticleItemBinding.inflate(inflater, parent, false);
-//        return new BlogViewHolder(binding);
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(@NonNull BlogViewHolder holder, int position) {
-//        BlogItemModel blogItem = blogList.get(position);
-//        holder.bind(blogItem);
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return blogList.size();
-//    }
-//
-//    public void setData(List<BlogItemModel> blogSavedList) {
-//        this.blogList = blogSavedList;
-//        notifyDataSetChanged();
-//    }
-//
-//    public class BlogViewHolder extends RecyclerView.ViewHolder {
-//        private final ArticleItemBinding binding;
-//
-//        public BlogViewHolder(ArticleItemBinding binding) {
-//            super(binding.getRoot());
-//            this.binding = binding;
-//        }
-//
-//        public void bind(BlogItemModel blogItem) {
-//            binding.heading.setText(blogItem.getHeading());
-//            Glide.with(binding.profile.getContext()).load(blogItem.getProfileImage()).into(binding.profile);
-//            binding.userName.setText(blogItem.getUserName());
-//            binding.date.setText(blogItem.getDate());
-//            binding.post.setText(blogItem.getPost());
-//
-//            binding.readMoreButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    itemClickListener.onReadMoreClick(blogItem);
-//                }
-//            });
-//            binding.editButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    itemClickListener.onEditClick(blogItem);
-//                }
-//            });
-//            binding.DeleteButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    itemClickListener.onDeleteClick(blogItem);
-//                }
-//            });
-//        }
-//    }
-//}
-
-
 package com.example.echopen.adapter;
+
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -107,7 +11,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.echopen.Model.BlogItemModel;
-import com.example.echopen.R;
 import com.example.echopen.databinding.ArticleItemBinding;
 
 import java.util.List;
@@ -140,7 +43,8 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.BlogView
 
     @Override
     public void onBindViewHolder(@NonNull BlogViewHolder holder, int position) {
-        holder.bind(blogList.get(position));
+        BlogItemModel blogItem = blogList.get(position);
+        holder.bind(blogItem);
     }
 
     @Override
@@ -153,15 +57,12 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.BlogView
         notifyDataSetChanged();
     }
 
-    public class BlogViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class BlogViewHolder extends RecyclerView.ViewHolder {
         private final ArticleItemBinding binding;
 
         public BlogViewHolder(ArticleItemBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
-            binding.readMoreButton.setOnClickListener(this);
-            binding.editButton.setOnClickListener(this);
-            binding.deleteButton.setOnClickListener(this);
         }
 
         public void bind(BlogItemModel blogItem) {
@@ -170,25 +71,27 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.BlogView
             binding.userName.setText(blogItem.getUserName());
             binding.date.setText(blogItem.getDate());
             binding.post.setText(blogItem.getPost());
-        }
 
-        @Override
-        public void onClick(View v) {
-            int position = getAdapterPosition();
-            if (position != RecyclerView.NO_POSITION) {
-                BlogItemModel blogItem = blogList.get(position);
-                switch (v.getId()) {
-                    case R.id.read_more_button:
-                        itemClickListener.onReadMoreClick(blogItem);
-                        break;
-                    case R.id.edit_button:
-                        itemClickListener.onEditClick(blogItem);
-                        break;
-                    case R.id.delete_button:
-                        itemClickListener.onDeleteClick(blogItem);
-                        break;
+            binding.readMoreButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    itemClickListener.onReadMoreClick(blogItem);
                 }
-            }
+            });
+            binding.editButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    itemClickListener.onEditClick(blogItem);
+                }
+            });
+            binding.DeleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    itemClickListener.onDeleteClick(blogItem);
+                }
+            });
         }
     }
 }
+
+
