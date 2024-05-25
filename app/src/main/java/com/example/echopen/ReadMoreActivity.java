7677a632a2e92370957a7dbbfe868f2821dc63a4
Forhad Islam Rony <forhadrony161@gmail.com>
2024-05-25 12:30:51 +0600

    SingleTon Desing pattern added in Readmore Activity and fixing some bugs

--- app/src/main/java/com/example/echopen/ReadMoreActivity.java
+++ app/src/main/java/com/example/echopen/ReadMoreActivity.java
@@ -1,7 +1,6 @@
 package com.example.echopen;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -9,12 +8,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.echopen.Model.BlogItemModel;
+import com.example.echopen.SingleTon.FirebaseDatabaseSingleton;
 import com.example.echopen.databinding.ActivityReadMoreBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.*;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class ReadMoreActivity extends AppCompatActivity {
 
@@ -42,12 +45,11 @@ public class ReadMoreActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        blogReference = FirebaseDatabase.getInstance("https://echopen-1e18e-default-rtdb.firebaseio.com/")
-                .getReference().child("blogs").child(postId);
+        blogReference = FirebaseDatabaseSingleton.getInstance().getDatabaseReference().child(postId);
 
         if (mAuth.getCurrentUser() != null) {
-            userSavedPostsReference = FirebaseDatabase.getInstance().getReference()
-                    .child("users").child(mAuth.getCurrentUser().getUid()).child("saveBlogPosts");
+            userSavedPostsReference = FirebaseDatabaseSingleton.getInstance().getUserReference()
+                    .child(mAuth.getCurrentUser().getUid()).child("saveBlogPosts");
         }
 
         binding.backButton.setOnClickListener(v -> finish());
